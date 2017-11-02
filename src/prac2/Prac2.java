930a047faaf954c4a167a7f63b825f48a496ae8a
Student <Student@PC83>
2017-11-02 15:45:38 +0800

    my first commit

--- src/prac2/Prac2.java
+++ src/prac2/Prac2.java
@@ -5,6 +5,7 @@ public class Prac2 {
     public static void main(String[] args) {
         System.out.println("Main");
         System.out.println("First-branch");
+        System.out.println("Hi");
     }
     
 }
